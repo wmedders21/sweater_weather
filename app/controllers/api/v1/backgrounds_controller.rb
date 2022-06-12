123407f7a9cca012c68f96c49e87c4dd7e1bf6a4
wmedders21 <wmedders@gmail.com>
2022-06-12 16:48:12 -0400

    Request Test: backgrounds index

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::BackgroundsController < ApplicationController
+
+  def index
+    binding.pry
+  end
+end
--- app/poros/background.rb
+++ app/poros/background.rb
@@ -1,15 +1,18 @@
 class Background
-  attr_reader :image, :credit
+  attr_reader :id, :type, :attributes
   def initialize(data)
-    @image = {
-      name: data[:name],
-      image_url: data[:contentUrl]
-    }
-
-    @credit = {
-      source: data[:hostPageUrl],
-      author: data[:hostPageDomainFriendlyName],
-      logo: data[:hostPageFavIconUrl]
+    @id = 'null'
+    @type = 'image'
+    @attributes = {
+      image: {
+        name: data[:name],
+        image_url: data[:contentUrl]
+      },
+      credit: {
+        source: data[:hostPageUrl],
+        author: data[:hostPageDomainFriendlyName],
+        logo: data[:hostPageFavIconUrl]
+      }
     }
   end
 end
