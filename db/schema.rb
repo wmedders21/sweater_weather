32eb827fa8e60e90cae26636455dc269e83c52f6
wmedders21 <wmedders@gmail.com>
2022-06-12 19:12:19 -0400

    Model Test: user validations

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2022_06_12_224841) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "pw_digest"
+    t.string "password_digest"
   end
 
 end
