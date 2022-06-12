78bd4fe6c9ded3daa75fa2de31b95f250b4707d3
wmedders21 <wmedders@gmail.com>
2022-06-12 14:14:53 -0400

    Setup bing api key

---
+++ app/services/bing_image_service.rb
@@ -0,0 +1,13 @@
+class BingImageService < BaseService
+  @url = "https://api.bing.microsoft.com/v7.0/images/search"
+
+  def self.conn(url, keyname, key)
+    Faraday.new(url: url) do |faraday|
+      faraday.headers[keyname] = key
+    end
+  end
+
+  def self.get_background(search_term)
+    get_json(conn(@url, 'Ocp-Apim-Subscription-Key', ENV['bing_images_key']).get("?q=#{search_term}&count=1&mkt=en-us&safeSearch=Strict"))
+  end
+end
