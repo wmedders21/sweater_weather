43a45529be2d2a7a261bf3f14b65096f823ad098
wmedders21 <wmedders@gmail.com>
2022-06-13 11:55:40 -0400

    More tests for book-search endpoint

--- spec/requests/api/v1/book-search/index_spec.rb
+++ spec/requests/api/v1/book-search/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe 'book-search endpoint' do
+Rspec.describe 'book-search endpoint', :vcr do
   before :each do
     get 'api/v1/book-search?location=denver,co&quantity=5'
     @response_body = JSON.parse(response.body, symbolize_names: true)
@@ -13,5 +13,31 @@ Rspec.describe 'book-search endpoint' do
     expect(@response_body[:data][:type]).to eq("books")
     expect(@response_body[:data][:id]).to eq("null")
     expect(@response_body[:data][:attributes]).to be_a(Hash)
-  end    
+  end
+
+  it 'contains attributes destination, forecast, total_books_found, and books' do
+    attributes = @response_body[:data][:attributes]
+
+    expect(attributes.keys.count).to eq(4)
+    expect(attributes.keys).to eq([:destination, :forecast, :total_books_found, :books])
+    expect(attributes[:destination]).to be_a(String)
+    expect(attributes[:forecast]).to be_a(Hash)
+    expect(attributes[:forecast].keys).to eq([:summary, :temperature])
+    expect(attributes[:forecast][:summary]).to be_a(String)
+    expect(attributes[:forecast][:temperature]).to be_a(String)
+    expect(attributes[:total_books_found]).to be_a(Integer)
+    expect(attributes[:books]).to be_a(Array)
+    expect(attributes[:books].count).to eq(5)
+  end
+
+  it 'returns a list of books' do
+    books = @response_body[:data][:attributes][:books]
+
+    books.each do |book|
+      expect(book.keys).to eq([:isbn, :title, :publisher])
+      expect(book[:isbn]).to be_a(Array)
+      expect(book[:title]).to be_a(String)
+      expect(book[:publisher]).to be_a(Array)
+    end
+  end      
 end
