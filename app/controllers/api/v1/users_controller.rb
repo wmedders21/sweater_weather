e74b6b766ff88d6821328be37b55185887401114
wmedders21 <wmedders@gmail.com>
2022-06-12 21:05:22 -0400

    endpoint to create new user happy path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
+    if user_params[:password] == user_params[:password_confirmation]
+      new_user = User.new(user_params)
+      if new_user.save
+        new_user.update(api_key: SecureRandom.hex)
+        render json: UserSerializer.new(new_user), status: 201
+      else
+        render json: {}
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'request to create a new user' do
+RSpec.describe 'request to create a new user, happy path' do
   it 'returns data hash with type, id, and attributes' do
     body = JSON.parse(File.read('./spec/fixtures/login_credentials.json'), symbolize_names: true)
     headers = {"CONTENT_TYPE" => "application/json"}
@@ -10,9 +10,9 @@ RSpec.describe 'request to create a new user' do
 
     expect(response.status).to eq(201)
     expect(response_body).to have_key(:data)
-    expect(response_body[:data].keys).to eq([:type, :id, :attributes])
+    expect(response_body[:data].keys).to eq([:id, :type, :attributes])
     expect(response_body[:data][:type]).to eq("users")
-    expect(response_body[:data][:id]).to be_a(Integer)
+    expect(response_body[:data][:id]).to be_a(String)
     expect(response_body[:data][:attributes]).to be_a(Hash)
     expect(response_body[:data][:attributes].keys).to eq([:email, :api_key])
     expect(response_body[:data][:attributes][:email]).to be_a(String)
