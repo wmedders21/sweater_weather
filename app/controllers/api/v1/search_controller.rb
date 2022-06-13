9c80d6963f8fb8e35b7ba47272abecfa27d53639
wmedders21 <wmedders@gmail.com>
2022-06-13 13:42:25 -0400

    booksearch endpoint Passes Sad path quantity less than 1

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SearchController < ApplicationController
+  before_action :check_quantity
 
   def books
     book_search = BookSearchFacade.search_books(search_params)
@@ -9,5 +10,11 @@ class Api::V1::SearchController < ApplicationController
 
   def search_params
     params.permit(:location, :quantity)
+  end
+
+  def check_quantity
+    if params[:quantity].to_i < 1
+      render json: { error: "Quantity can't be less than 1"}, status:400
+    end
   end
 end
