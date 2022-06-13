cf8c6bcf9573607dfdc345e3a08c9a0a76483d32
wmedders21 <wmedders@gmail.com>
2022-06-13 12:56:13 -0400

    book-search endpoint passes happy path tests

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SearchController < ApplicationController
 
   def books
     book_search = BookSearchFacade.search_books(search_params)
+    render json: BookSearchSerializer.new(book_search)
   end
 
   private
