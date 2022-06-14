8fe6cc5ba137fade14ab4683c7ee953f21ad36a3
wmedders21 <wmedders@gmail.com>
2022-06-13 20:54:32 -0400

    Request sad path: create user with mismatched pw returns error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :check_passwords
 
   def create
-    if user_params[:password] == user_params[:password_confirmation]
-      new_user = User.new(user_params)
-      if new_user.save
-        new_user.update(api_key: SecureRandom.hex)
-        render json: UserSerializer.new(new_user), status: 201
-      else
-        render json: {}
-      end
+    new_user = User.new(user_params)
+    if new_user.save
+      new_user.update(api_key: SecureRandom.hex)
+      render json: UserSerializer.new(new_user), status: 201
     end
   end
 
@@ -16,5 +13,11 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def check_passwords
+    if user_params[:password] != user_params[:password_confirmation]
+      render json: { error: "Invalid Credentials" }, status: 401
+    end
   end
 end
