1c4277b6598f651fdb2e27221b76ae87fd5da4ea
wmedders21 <wmedders@gmail.com>
2022-06-14 16:00:51 -0400

    Model Test: User validates presence of :api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
+  validates_presence_of :api_key
 
   has_secure_password
 end
