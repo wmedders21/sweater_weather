be82daa1cebcf484106bf140ab24630a6e70550c
wmedders21 <wmedders@gmail.com>
2022-06-14 17:06:17 -0400

    Request roadtrip endpoint sad path no destination

--- spec/requests/api/v1/road_trip/index_spec.rb
+++ spec/requests/api/v1/road_trip/index_spec.rb
@@ -83,5 +83,16 @@ RSpec.describe 'road_trip endpoint' do
       expect(response.status).to eq(401)
       expect(response_body[:error]).to eq("Missing Field")
     end
+
+    it 'sad path no destination' do
+      body = { origin: "Pueblo, CO", api_key: "#{@user.api_key}" }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v1/road_trip', headers: headers, params: JSON.generate(body)
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(401)
+      expect(response_body[:error]).to eq("Missing Field")
+    end
   end
 end
